51e7a523a508ce88241e02567e7746e880bab172
Graham Knop <haarg@haarg.org>
2019-04-02 15:38:51 +0200

    more edge cases to test

---
+++ t/env/edges.pl
@@ -0,0 +1,12 @@
+{
+  "OPTION_01" => 1,
+  "OPTION_02" => 2,
+  "OPTION_03" => 3,
+  "OPTION_04" => "123welp\"arg",
+  "OPTION_05" => "qu\\oted",
+  "OPTION_06" => "qu\\oted",
+  "OPTION_07" => "no quotes here",
+  "OPTION_08" => "escapesomestuff",
+  "OPTION_09" => "not-a#comment",
+  "OPTION_10" => "yes-a",
+}
